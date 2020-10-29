e7e8849b324cab243b127b6796b57cbdfdbe2799
neatomaru <neatomaru@gmail.com>
2020-10-30 02:05:30 +0300

    add menuBar and opportunity to change Test's name

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import './App.css';
 import {Route, Switch, BrowserRouter} from 'react-router-dom'
 import QuizList from "./containers/QuizList";
 import Quiz from "./containers/Quiz";
+import MenuBar from "./components/MenuBar/MenuBar";
+import TestsList from "./containers/TestsList";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+
         <BrowserRouter>
+                <MenuBar />
             <Switch>
+                <Route path='/editor' component={TestsList} />
                 <Route path='/test/:id' component={Quiz} />
                 <Route path='/' component={QuizList} exact/>
             </Switch>
